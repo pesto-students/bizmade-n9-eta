aaab8c36ee36c0571d72aa2810b0e0b20cbfae83
VenkataLeela02 <33417967+VenkataLeela02@users.noreply.github.com>
2021-12-13 23:39:31 +0530

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -122,6 +122,14 @@ function HomeScreen({ match, history }) {
                           All
                         </div>
                         <div
+                          className="sports"
+                          name="fq"
+                          data-value="sports"
+                          onClick={(e) => categoryHandler(e)}
+                        >
+                          Sports
+                        </div>
+                        <div
                           className="clothes"
                           name="fq"
                           data-value="Clothing"
@@ -152,14 +160,6 @@ function HomeScreen({ match, history }) {
                           onClick={(e) => categoryHandler(e)}
                         >
                           Machinery
-                        </div>
-                        <div
-                          className="sports"
-                          name="fq"
-                          data-value="sports"
-                          onClick={(e) => categoryHandler(e)}
-                        >
-                          Sports
                         </div>
                         <div
                           className="home"
